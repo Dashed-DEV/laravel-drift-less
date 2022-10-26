8aec3ffe1ffa830e0caadfba04cfb95352ee0c3b
Robin van Maasakker <robin@qubiqx.com>
2022-10-26 11:11:43 +0200

    Update

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('__images/{configName}/{manipulations}/{path}', ImagesController::class)
     ->where('path', '.*')
-    ->name('__images.manipulate')
-    ->middleware('signed');
+    ->name('__images.manipulate');
